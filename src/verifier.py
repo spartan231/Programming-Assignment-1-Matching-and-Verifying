8f524f4fa019b8aad2877184bb6dc337670596d0
ACao <avcao2003@gmail.com>
2026-01-29 16:36:21 -0500

    new verify file for task b

---
+++ src/verifier.py
@@ -0,0 +1,13 @@
+def check_validity(hospital, student, match):
+    """
+    check the validity of the provided data.
+
+    parameters:
+    hospital : The hospital data to be checked.
+    student : The student data to be checked.
+    match : The match data to be checked.
+
+    returns:
+    bool: True if match is valid and stable, False otherwise.
+    """
+    return 0
