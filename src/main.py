53be7068e0f1924282ac1fb01cc5bb81a7fc37c5
spartan231 <howardmilleriv23@gmail.com>
2026-01-29 23:17:10 -0500

    Added the Matching Engine for the project

--- src/main.py
+++ src/main.py
@@ -2,17 +2,22 @@ from matchingEngine import MatchingEngine
 
 def main():
     hospitals = {
-        "h1": ["a", "b"],
-        "h2": ["a", "b"]
+        "h1": ["a", "b", "c"],
+        "h2": ["a", "b", "c"],
+        "h3": ["b", "a", "c"]
     }
 
     students = {
-        "s1": ["h1", "h2"],
-        "s2": ["h2", "h1"]
+        "a": ["h1", "h2", "h3"],
+        "b": ["h2", "h1", "h3"],
+        "c": ["h1", "h2", "h3"]
     }
 
 
-    MatchingEngine(hospitals, students)
+    matches = MatchingEngine(hospitals, students)
+    for h,s in matches.items():
+        print(f"{h} {s}")
+    
     return 0
 
 main()
--- src/matchingEngine.py
+++ src/matchingEngine.py
@@ -14,8 +14,48 @@ def MatchingEngine(hospital, student):
     hospitals_free = list(hospital.keys())
     students_free = list(student.keys())
 
-
-    # print("H -> ", hospitals_free)
-    # print("S -> ", students_free)
-
+    hospital_matches = {}
+    student_matches = {}
     #TODO: now finish up the rest of the algorithm
+    while len(hospitals_free) > 0:
+        # pick the first free hospital
+        h = hospitals_free[0]
+        
+        try:
+            # get highest ranked student
+            s = hospital[h].pop(0)  
+        except IndexError:
+            # hospital has no more students to propose to
+            hospitals_free.pop(0)
+            continue
+        
+        if (s not in student):
+            print(f"Student {s} not found in student list.")
+            continue
+
+        if (s in students_free):
+            students_free.remove(s)
+            hospitals_free.remove(h)
+            hospital_matches[h] = s
+            student_matches[s] = h
+            
+        else:
+            # student has been matched
+            # find out who they are matched to
+            curr_hospital = student_matches[s]
+            # student preference list
+            s_prefs = student[s]
+
+            if(s_prefs.index(h) < s_prefs.index(curr_hospital)):
+                # student prefers new hospital
+                hospital_matches[h] = s
+                student_matches[s] = h
+                hospitals_free.remove(h)
+                hospitals_free.append(curr_hospital)
+                del hospital_matches[curr_hospital]
+                
+            else:
+                # student prefers current hospital
+                continue
+    return hospital_matches
+        
